bc8a5c24c5e81432cc6ba6e7ce04f0c1e6772e81
rafaeldiasgonca <rafaeldiasgonca@gmail.com.br>
2020-07-06 15:44:08 -0400

    Initial Commit

---
+++ bestPlayers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bestPlayers
+//
+//  Created by Rafael Dias Gonçalves on 06/07/20.
+//  Copyright © 2020 Rafael Dias Gonçalves. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
